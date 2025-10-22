5e4db19f4d0c8fdbfd33fda9d25d537dd9ce89d6
JasonDebnath001 <aritradebnath1@gmail.com>
2025-10-22 13:57:43 +0530

    Initial project setup

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require("express");
+const colors = require("colors");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+// .env config
+dotenv.config();
 
 //rest object
 const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
 //port
 app.get("/", (req, res) => {
   return res.status(200).send("<h1>Welcome To Server App</h1>");
 });
 
-const PORT = 8000;
-
-app.listen(PORT, () => {
-  console.log("Server is running");
+app.listen(process.env.PORT, () => {
+  console.log("Server is running".bgMagenta.white);
 });
