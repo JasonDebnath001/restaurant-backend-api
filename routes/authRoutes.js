50176e5a31c6e75d9d4621a2d5453ba394dfd746
JasonDebnath001 <aritradebnath1@gmail.com>
2025-10-29 18:57:55 +0530

    Implements Login functionality

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const { registerController } = require('../controllers/authController')
+const express = require("express");
+const {
+  registerController,
+  loginController,
+} = require("../controllers/authController");
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/register', registerController)
+router.post("/register", registerController);
+router.post("/login", loginController);
 
-module.exports = router
+module.exports = router;
